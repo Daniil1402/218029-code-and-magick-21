05698f57f658e8d62be9aba45c0631bcbfeab543
Daniil1402 <yadanya14@gmail.com>
2020-10-10 20:25:37 +0300

    +fixes

--- js/setup.js
+++ js/setup.js
@@ -61,11 +61,11 @@ userNameInput.addEventListener(`input`, function () {
 
   if (valueLength < MIN_NAME_LENGTH) {
     userNameInput.setCustomValidity(
-      `Ещё ` + (MIN_NAME_LENGTH - valueLength) + ` симв.`
+        `Ещё ` + (MIN_NAME_LENGTH - valueLength) + ` симв.`
     );
   } else if (valueLength > MAX_NAME_LENGTH) {
     userNameInput.setCustomValidity(
-      `Удалите лишние ` + (valueLength - MAX_NAME_LENGTH) + ` симв.`
+        `Удалите лишние ` + (valueLength - MAX_NAME_LENGTH) + ` симв.`
     );
   } else {
     userNameInput.setCustomValidity(``);
